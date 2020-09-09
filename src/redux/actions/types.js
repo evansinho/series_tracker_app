2244d0bc21eabbba3c9c339157d3e796a80de45a
evanson <igiri.evanson@gmail.com>
2020-09-10 00:13:52 +0100

    create an auth reducer

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,2 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const REGISTER_SUCCESSFUL = 'REGISTER_SUCCESSFUL';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
